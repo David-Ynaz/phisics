b6ae8c8301c2def3756afcc313cc0e55ad2ea6d2
David-Ynaz <davukr@outlook.com>
2022-10-13 12:15:10 +0400

    1.0.0.2

--- physic/Program.cs
+++ physic/Program.cs
@@ -2,3 +2,10 @@
 int metr = int.Parse(Console.ReadLine());
 Console.WriteLine("Сантиметры: "+ metr * 100);
 Console.WriteLine("Километры: "+ metr * 0.001);
+
+
+Console.WriteLine("Введите граммы: ");
+int gram = int.Parse(Console.ReadLine());
+double galon = gram * 0.001;
+Console.WriteLine("Литров: "+ galon);
+Console.WriteLine("галлоны: "+ gram * 0.001);
