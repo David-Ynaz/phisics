c688911929d7b59382c03f3b4537997325bbfa42
David-Ynaz <davukr@outlook.com>
2022-10-13 12:18:09 +0400

    Версия 1.0.0.3

--- physic/Program.cs
+++ physic/Program.cs
@@ -8,4 +8,9 @@ Console.WriteLine("Введите граммы: ");
 int gram = int.Parse(Console.ReadLine());
 double galon = gram * 0.001;
 Console.WriteLine("Литров: "+ galon);
-Console.WriteLine("галлоны: "+ gram * 0.001);
+Console.WriteLine("галлоны: "+ gram * 0.219969);
+
+
+Console.WriteLine("Введите цельсии: ");
+int cel = int.Parse(Console.ReadLine());
+Console.WriteLine("Фаренгейт: "+ cel * 9/5+32);
